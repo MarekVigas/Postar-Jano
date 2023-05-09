9259de24abd3f7b1bada36084ef3d2c2fc200e39
Marek Vigas <vigas147@gmail.com>
2023-05-09 12:42:16 +0200

    fix radios forgetting state

--- fe/src/components/from/radio.tsx
+++ fe/src/components/from/radio.tsx
@@ -1,5 +1,5 @@
 import { IonRadio, IonRadioGroup } from "@ionic/react";
-import { UseFormRegister, UseFormSetValue } from "react-hook-form";
+import { UseFormGetValues, UseFormRegister, UseFormSetValue } from "react-hook-form";
 import { Registration } from "../../utils/types";
 
 type RadioOption = {
@@ -10,14 +10,17 @@ type RadioOption = {
 type RadioProps = {
   register: UseFormRegister<Registration>
   setValue: UseFormSetValue<Registration>
+  getValues: UseFormGetValues<Registration>
   name: any
   options: RadioOption[]
 }
 
-export const RadioGroup: React.FC<RadioProps> = ({ register, setValue, name, options }) => (
+export const RadioGroup: React.FC<RadioProps> = ({ register, setValue, getValues, name, options }) => (
   <IonRadioGroup
     style={{ display: 'flex', flexDirection: 'column', width: '100%', justifyContent: 'space-between', height: '6em' }}
     {...register(name)}
+    defaultValue={getValues(name)}
+    value={getValues(name)}
     onIonChange={e => setValue(name, e.detail.value)}
   >
     {
