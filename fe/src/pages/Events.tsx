b5295f321fd7626b37dc4f21907d7476fdfc9e2e
Marek Vigas <vigas147@gmail.com>
2023-05-09 14:44:55 +0200

    add better event list with promo status

--- fe/src/pages/Events.tsx
+++ fe/src/pages/Events.tsx
@@ -1,6 +1,6 @@
-import { IonButtons, IonContent, IonHeader, IonItem, IonLoading, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonItem, IonLabel, IonList, IonLoading, IonMenuButton, IonPage, IonThumbnail, IonTitle, IonToolbar } from '@ionic/react';
 import useSWR from 'swr';
-import { IEvent } from '../utils/types';
+import { IEvent, PromoResponse } from '../utils/types';
 import queryString from 'query-string'
 import { useLocation } from 'react-router-dom'
 import { useEffect } from 'react';
@@ -17,7 +17,16 @@ const EventsPage: React.FC = () => {
   const { data: events, isLoading } = useSWR<IEvent[]>({ url: `/events` })
   const { search } = useLocation()
   const { promo } = queryString.parse(search)
-  const { actions } = useStateMachine({ updatePromoCode })
+  const { actions, state } = useStateMachine({ updatePromoCode })
+
+  const { data: promoValidation } = useSWR<PromoResponse>(state.promo ? {
+    url: `promo_codes/validate`, config: {
+      method: 'POST',
+      data: {
+        promo_code: state.promo
+      }
+    }
+  } : null)
 
   useEffect(() => {
     if (promo) {
@@ -48,12 +57,32 @@ const EventsPage: React.FC = () => {
           title='Načítavam...'
         />
         {
-          events && events.map((event, i) => (
-            <IonItem key={i} href={`/event/${event.id}`}>
-              {event.title}
-            </IonItem>
-          )
-          )
+          state.promo && promoValidation && promoValidation.status == "ok" &&
+          <IonItem>
+            Váš promo kód: {state.promo} <br />
+            Zostávajúci počet použití: {promoValidation?.available_registrations}
+          </IonItem>
+        }
+        {
+          events &&
+          <IonCard>
+            <IonCardHeader>
+              <IonCardTitle>Akcie</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <IonList>
+                {events.map((event) => (
+                  <IonItem href={`/event/${event.id}`} key={event.id}>
+                    <IonThumbnail slot="start">
+                      <img alt={`${event.title} photo`} src={event.photo} />
+                    </IonThumbnail>
+                    <IonLabel>{event.title}</IonLabel>
+                    <IonButton slot='end'>Prihlásiť</IonButton>
+                  </IonItem>
+                ))}
+              </IonList>
+            </IonCardContent>
+          </IonCard>
         }
       </IonContent>
     </IonPage>
