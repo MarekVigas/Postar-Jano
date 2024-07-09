df44ecbe966268080aafb2b611194385e073a2c4
Marek Vigas <vigas147@gmail.com>
2024-07-09 21:15:02 +0200

    remove old shit

--- build-trnavka.sh
+++
@@ -1,8 +0,0 @@
-docker build ./db -t marekvigas/sbb-leto-db:trnavka
-docker push marekvigas/sbb-leto-db:trnavka
-docker build ./admin -t marekvigas/sbb-leto-admin:trnavka
-docker push marekvigas/sbb-leto-admin:trnavka
-docker build ./src/go-api -t marekvigas/sbb-leto-api:trnavka
-docker push marekvigas/sbb-leto-api:trnavka
-docker build ./fe --build-arg REACT_APP_API_HOST=https://tabory.trnavka.sk --build-arg REACT_APP_RESULT_REDIRECT=https://trnavka.sk/tabory -t marekvigas/sbb-leto-form:trnavka
-docker push marekvigas/sbb-leto-form:trnavka
